052709d718e2e8a01573fca380364cb2b2032755
benedict <benedict@benedict-ThinkPad-Edge-E530.fritz.box>
2015-05-28 19:59:18 +0200

    Inital import der Package struktur: org.ydownloader.*

---
+++ src/org/ydownloader/Main.java
@@ -0,0 +1,23 @@
+package org.ydownloader;
+
+import java.awt.EventQueue;
+
+import org.ydownloader.gui.GUI;
+
+public class Main {
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
