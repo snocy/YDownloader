a0df8d68feb72d315684b82d67295c05eadb2910
benedict <benedict@benedict-ThinkPad-Edge-E530>
2015-06-26 10:11:24 +0200

    Interface implementiert

---
+++ src/org/ydownloader/Downloader.java
@@ -0,0 +1,11 @@
+package org.ydownloader;
+
+import java.io.File;
+
+public interface Downloader {
+	public boolean DownloadFinished();
+	public int getProgress();
+	public File getPath();
+	public String getUrl();
+	public void run(String url, File path);
+}
