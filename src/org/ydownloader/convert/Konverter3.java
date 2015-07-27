e5668d0f63b53c608eb87eb9b6de1be66c795cef
Engel266 <stefanengelspam@gmail.com>
2015-07-27 14:52:18 +0200

    Konverter Verlauf Progress

--- src/org/ydownloader/convert/Konverter3.java
+++ src/org/ydownloader/convert/Konverter3.java
@@ -3,16 +3,23 @@ package org.ydownloader.convert;
 import it.sauronsoftware.jave.AudioAttributes;
 import it.sauronsoftware.jave.Encoder;
 import it.sauronsoftware.jave.EncoderException;
+import it.sauronsoftware.jave.EncoderProgressListener;
 import it.sauronsoftware.jave.EncodingAttributes;
 import it.sauronsoftware.jave.InputFormatException;
 
+
+
+import it.sauronsoftware.jave.MultimediaInfo;
+
 import java.io.File;
 
-public class Konverter3 {
-	
+public class Konverter3   {
+public int progress;	
 	public Konverter3()  {
 		// TODO Auto-generated constructor stub
 	}
+	
+	
 	
 	public boolean Konvert (File source, String targetstream) throws IllegalArgumentException, InputFormatException, EncoderException{
 		//File source = new File(sourcestream);
@@ -25,16 +32,40 @@ public class Konverter3 {
 		EncodingAttributes attrs = new EncodingAttributes();
 		attrs.setFormat("mp3");
 		attrs.setAudioAttributes(audio);
+		EncoderProgressListener listen = new EncoderProgressListener() {
+			
+			@Override
+			public void sourceInfo(MultimediaInfo arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void progress(int pr) {
+				progress=pr;
+				
+			}
+			
+			@Override
+			public void message(String arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		};
 		Encoder encoder = new Encoder();
-		encoder.encode(source, target, attrs);
-		System.out.println("Durch gegangen");
+		encoder.encode(source, target, attrs,listen);
 		return true;
 	}
+
+		
+		
 	
 	public static void main(String[] args) throws InputFormatException, EncoderException {
 		
 				
 	}
+
+	
 
 }
 
