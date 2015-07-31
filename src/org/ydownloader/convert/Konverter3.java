9d7a3354413820bfd555f641cd2f9ac79a56c2bf
ra1dbossKAPPA <osterried.simon@gmail.com>
2015-07-31 22:05:50 +0200

    Wie abgesprochen mit der Doku und meinen Kommentaren zu Gui und Konverter.

--- src/org/ydownloader/convert/Konverter3.java
+++ src/org/ydownloader/convert/Konverter3.java
@@ -14,24 +14,28 @@ import it.sauronsoftware.jave.MultimediaInfo;
 import java.io.File;
 
 public class Konverter3   {
+//Progress Variable um den Fortschritt anzuzeigen
 public int progress;	
 	public Konverter3()  {
 		// TODO Auto-generated constructor stub
 	}
 	
 	
-	
+	//Konvert Function die die Ausgangsdatei und den Zielspeicherort übergeben bekommt. Die Funktionen kommen aus dem Sauronsoftware package.
 	public boolean Konvert (File source, String targetstream) throws IllegalArgumentException, InputFormatException, EncoderException{
 		//File source = new File(sourcestream);
 		File target = new File(targetstream);
 		AudioAttributes audio = new AudioAttributes();
+		//Im folgenden werden die Audioattribute für die Konvertierung gesetzt. U. a. der Codec und  die Bitrate
 		audio.setCodec("libmp3lame");
 		audio.setBitRate(new Integer(128000));
 		audio.setChannels(new Integer(2));
 		audio.setSamplingRate(new Integer(44100));
+		//Hier wird die Konvertierungsattribute gesetzt
 		EncodingAttributes attrs = new EncodingAttributes();
 		attrs.setFormat("mp3");
 		attrs.setAudioAttributes(audio);
+		//Ein progress Listener, namer erklärt seine Funktion
 		EncoderProgressListener listen = new EncoderProgressListener() {
 			
 			@Override
@@ -52,6 +56,8 @@ public int progress;
 				
 			}
 		};
+		//Nun wird ein Encoder Objekt erstellt und anschließend die Funktion Aufgerufen die sich um die Konvertierung
+		//der Datei in MP3 kümmert.
 		Encoder encoder = new Encoder();
 		encoder.encode(source, target, attrs,listen);
 		return true;
--- src/org/ydownloader/gui/GUI.java
+++ src/org/ydownloader/gui/GUI.java
@@ -54,6 +54,7 @@ import java.util.Queue;
 
 public class GUI {
 
+	//Hier werden die für die GUI relevanten Variablen definiert
 	public JFrame frame;
 	private JTextField link;
 	private JLabel lbleingefuegtelinnks;
@@ -61,8 +62,10 @@ public class GUI {
 	private DefaultListModel model;
 	private String speicherort;
 	private Youtube you=null;
+	//Diese Variablen werden für die Progressbar benötigt
 	private int downloads=0;
 	private int downloadsfertig=0;
+	//erstellen eines leeren Konverterobjekts das später für die Konvertierung benötigt wird
 	private Konverter3 kv=null;
 	public JLabel lblaktuell;
 	/**
@@ -83,6 +86,7 @@ public class GUI {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		//Initialisieren der GUI hier wird alles an seinen Platz gesetzt
 		frame = new JFrame();
 		frame.setBounds(100, 100, 820, 460);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -100,6 +104,7 @@ public class GUI {
 		btnlinkhinzufuegen.setBounds(12, 107, 150, 25);
 		frame.getContentPane().add(btnlinkhinzufuegen);
 		
+		//Sobald der Nutzer den Button Link hinzüfugen klickt wird der in Textfield eingetragene Link in die Downloadliste eingefügt.
 		btnlinkhinzufuegen.addActionListener(new ActionListener()
 	    {
 	        public void actionPerformed(ActionEvent e)
@@ -111,6 +116,7 @@ public class GUI {
 		JButton btnherunterladen = new JButton("Herunterladen");
 		btnherunterladen.setBounds(12, 383, 180, 25);
 		frame.getContentPane().add(btnherunterladen);
+		//Startet die Herunterlade Funktion wenn der User sie anklickt.
 		btnherunterladen.addActionListener(new ActionListener()
 	    {
 	        public void actionPerformed(ActionEvent e)
@@ -136,7 +142,7 @@ public class GUI {
 		list = new JList(model);
 		list.setBounds(12, 190, 754, 100);
 		frame.getContentPane().add(list);
-		
+		//Einfügen der Progressbar die uns den Fortschritt des Downloads anzeigt.
 		JProgressBar progressBargesamt = new JProgressBar();
 		progressBargesamt.setBounds(12, 356, 778, 14);
 		frame.getContentPane().add(progressBargesamt);
@@ -144,11 +150,11 @@ public class GUI {
 		JProgressBar progressBareinzel = new JProgressBar();
 		progressBareinzel.setBounds(12, 318, 778, 14);
 		frame.getContentPane().add(progressBareinzel);
-		
+		//Scrollbar um sich die Liste der Links anzusehen die auf den Download warten.
 		JScrollBar scrollBar = new JScrollBar();
 		scrollBar.setBounds(769, 190, 21, 100);
 		frame.getContentPane().add(scrollBar);
-		
+		//Gesamt und Aktuell sind die Beschreibung der beiden Progressbars.
 		JLabel lblgesamt = new JLabel("Gesamt:");
 		lblgesamt.setBounds(12, 340, 90, 16);
 		frame.getContentPane().add(lblgesamt);
@@ -156,14 +162,14 @@ public class GUI {
 		lblaktuell = new JLabel("Aktuell:");
 		lblaktuell.setBounds(12, 303, 600, 16);
 		frame.getContentPane().add(lblaktuell);
-		
+		//Erstellen der Menubar in der der User den Speicherort der Datei auswählt oder auch das das Programm schließen.
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.setBounds(12, 10, 119, 26);
 		frame.getContentPane().add(menuBar);
-		
+		//Name des Punktes in der Menüleiste
 		JMenu mnNewMenudatei = new JMenu("Datei");
 		menuBar.add(mnNewMenudatei);
-		
+		//Der Unterpunkt des Menüpunktes Datei ist Schließen. Hier gleich mit Actionlistener er das Programm dann schließt.
 		JMenuItem mntmNewMenuItemschliessen = new JMenuItem("Schlie\u00DFen");
 		mntmNewMenuItemschliessen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -185,7 +191,7 @@ public class GUI {
 		});
 		mnNewMenudatei.add(mntmNewMenuItemordnereoffnen);
 		mnNewMenudatei.add(mntmNewMenuItemschliessen);
-		
+		//Zweiter Menüpunkt Optionen in dem sich u. a. der Speicherort einstellen lässt.
 		JMenu mnNewMenuoptionen = new JMenu("Optionen");
 		menuBar.add(mnNewMenuoptionen);
 		
@@ -201,15 +207,18 @@ public class GUI {
 		
 		
 		mnNewMenuoptionen.add(mntmNewMenuItemspeicherort);
-		
+		//Diese Funktion steuert die Progressbar, läuft als Thread neben dem Download
 		 class ProgressBar implements Runnable {
 		        public void run() {
 		        	while(true) try {
 		        		if(you!=null) {
-		        			progressBareinzel.setValue((int)(you.getProgress()));	
+		        			//Holt sich den Download Fortschritt aus der Youtube Klasse.
+		        			progressBareinzel.setValue((int)(you.getProgress()));
+		        			//Berechnet den aktuellen Wert den die Progressbargesamt hat basierend auf den Verbleibenden Downloads.
 		        			progressBargesamt.setValue((int)(downloadsfertig/downloads * 100) + (int)(you.getProgress()/downloads));
 		        			
 		        		} else {
+		        			//Wenn kein Download läuft dann ist die Progressbar bei 0
 		        			progressBareinzel.setValue(0);	
 		        			progressBargesamt.setValue(0);
 		        		}
@@ -227,8 +236,10 @@ public class GUI {
 	}
 	private static void addPopup(Component component, final JPopupMenu popup) {
 	}
+	//Funktion für das Einfügen der Links in die Downloadliste
 	private void addListe() {
 		int pos = list.getModel().getSize();
+		//Wenn der Link ein valider YOutube link ist wird er zur Liste hinzugefügt
 		if(new Youtube().validYoutube(this.link.getText())) {
 			model.add(pos, this.link.getText());
 		}
@@ -236,14 +247,16 @@ public class GUI {
 	}
 	private void startDL() {
 		if(speicherort==null) {
+			//Fehler wenn kein Speicherort angegeben ist
 			JOptionPane.showMessageDialog(null, "Bitte einen Speicherort unter Optionen einstellen!", "Speicherort-Warnung", JOptionPane.WARNING_MESSAGE);
 			return;
 		}
+		//Linkedlist für die Download Queue
 		Queue queue = new LinkedList();
 		
 		int dls = model.getSize();
 		
-		
+		//Einfügen in die queue
 		for(int i=0;i<dls;i++) {
 			queue.add(model.getElementAt(i).toString());
 			
@@ -252,23 +265,30 @@ public class GUI {
 		model.clear();
 		downloads=queue.size();
 		downloadsfertig=0;
+		//Start des Programmes wieder als Thread
 			class MyDownloader implements Runnable {
 					Queue dls;
 			        MyDownloader(Queue p) { dls = p; }
 			        public void run() {
+			        	//Läuft solange etwas in der Queue ist
 			        	while(dls.size()!=0) {
 			        	try {
-			        			
+			        			//Das Programm holt sich den Link des nächsten Elements aus der Liste
 			        			String ele = (String) dls.element();
 			        			System.out.println(dls.element() +" = PFAD =>"+ speicherort +ele.substring(ele.length() - 6)  + ".dl");
-			        			lblaktuell.setText("Aktuell: " + ele);
+			        			//lblaktuell zeigt den Status des Downloaders an
+			        			lblaktuell.setText("Aktuell: " + ele);			        		
 			        			you = new Youtube();
+			        			//Speichername der neuendatei ist ein Verkürzter Teil der Youtube URL
 			        			File path = new File( speicherort + ele.substring(ele.length() - 6)   + ".dl");
+			        			//Start des Downloads
 			        			you.run(ele, path);  
 			        			lblaktuell.setText("Aktuell: Konvertierung von " + ele);
 			        			kv = new Konverter3();
+			        			//Start des Konverters
 			        			kv.Konvert(path,speicherort + ele.substring(ele.length() - 6)+ ".mp3");
 			        			downloadsfertig++;
+			        			//Löscht den Link aus der Queue
 			        			dls.remove();
 			        			lblaktuell.setText("Aktuell:");
 						} catch (IOException e) {
@@ -282,12 +302,14 @@ public class GUI {
 							e.printStackTrace();
 						}
 			        	}
+			        	//Wenn die Queue Leer ist dann ist das Programm fertig.
 			        	you=null;
 			        	JOptionPane.showMessageDialog(null, "Deine Downloads sind heruntergeladen!", "Downloads Fertig", JOptionPane.OK_CANCEL_OPTION);
 
 
 			        }
 			    }
+			//ERstellt neuen Thread für den Downloader
 			Thread thread1 = new Thread(new MyDownloader(queue));
 			thread1.start();		
 		
